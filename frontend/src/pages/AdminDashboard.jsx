fa4958bfcc49dadbc04372f15d0b83cf221752f0
root <root@LAPTOP-HNMFBH9L.localdomain>
2026-01-29 00:59:42 +0500

    Fix syntax error in AdminDashboard.jsx

--- frontend/src/pages/AdminDashboard.jsx
+++ frontend/src/pages/AdminDashboard.jsx
@@ -477,7 +477,7 @@ export default function AdminDashboard() {
                       ))}
                     </>
                   )
-                }))}
+                })}
               </tbody>
             </table>
           </div>
